e94e2904a2ad40dd281c2dd696a947055e77b2ac
KeshavChib <109231157+KeshavChib@users.noreply.github.com>
2023-07-12 07:07:30 +0530

    Update Power.cpp
    Changed the power function

--- Power.cpp
+++ Power.cpp
@@ -1,12 +1,11 @@
-// The Function to calculate the power of a function in O(log(n)) time and space using Binary Exponention.............................
+// The Function to calculate the power of a function in O(log(n)) time and space using Binary Exponentiation.............................
 
-long long power(int N,int R)
-{
-   //Your code here
-    if(R == 0) return 1;
-       
-    long long ans = 1;
-    long long know = power(N, R/2)%mod;
-    if(R%2) ans = N;
-    return (ans*((know*know)%mod))%mod;
-}
+    long long power(int N,int R)
+    {
+       //Your code here
+        if(R == 0) return 1;
+        long long half = power(N, R/2);
+        long long ans = (half * half)%mod;
+        if(R%2) ans = (ans * N)%mod;
+        return ans;
+    }
